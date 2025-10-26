aa6deb28c0fc12fcbceab1af2ff30ed8ccf3c543
qAxelFT <axelfloresteja@protonmail.com>
2025-10-26 03:07:15 -0600

    Fix: agregado comentarios

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import flet as ft
 
 def main(page: ft.Page):
+    # TODO: completar la funcion para revisar la respuesta del usuario
     def check_answer():
         pass
 
+    # esta variable trackea la cuenta del score
     score = ft.Text("0", size=25, data=0)
+    # esta variable trackea la clave actual
     code = ft.Text("placeholder", size = 75)
+    # esta variable contiene la respuesta del usuario
     answer = ft.TextField(label="answer", text_align=ft.TextAlign.CENTER, width=500)
+    # el boton para mandar la respuesta
     submit_button = ft.ElevatedButton("Submit", on_click=check_answer)
 
+    # construccion de la interfaz
     page.add(
         ft.Container(
             content=
