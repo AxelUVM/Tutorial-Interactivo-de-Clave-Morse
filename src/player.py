7d1017cd49999fbbcc3e0b447fbec3c00d153c65
qAxelFT <axelfloresteja@protonmail.com>
2025-11-05 15:04:32 -0600

    Feat: add player logic

---
+++ src/player.py
@@ -0,0 +1,65 @@
+class Player():
+    morse_dict = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----."
+    }
+
+    def __init__(self, score: int = 0, streak: int = 0, mult: float = 1.0):
+        self.score = score
+        self.streak = streak
+        self.mult = mult
+
+    def check_answer(self, answer: str, code: str):
+        try:
+            if self.morse_dict[answer.upper()] == code:
+                self.score += 100
+                self.streak += 1
+                self.mult += 0.5
+            elif self.morse_dict[answer.upper()] != code and self.score > 0:
+                self.score -= 100
+                self.streak = 0
+                self.mult = 1
+            else:
+                self.score = 0
+                self.streak = 0
+                self.mult = 1
+        except KeyError:
+            self.score -= 100
+            self.streak = 0
+
+    def apply_mult(self):
+        return self.score * self.mult
