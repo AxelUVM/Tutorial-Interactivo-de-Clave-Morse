f379171397daad7ba9e64538e5cbb11d732934a6
qAxelFT <axelfloresteja@protonmail.com>
2025-11-05 15:03:49 -0600

    Feat: add score tests

--- tests/score_test.py
+++ tests/score_test.py
@@ -1,10 +1,28 @@
 import unittest
-from src.main import Player
-
-player = Player()
-player_expected = Player(100, 1)
+from src.player import Player
 
 class TestScore(unittest.TestCase):
-    def test_add(self):
-        # Checar si respuesta del usuario (arg1) es igual al codigo actual (arg2) y suma 1 al score
-        self.assertEqual(, pla)
+    def test_right_letter(self):
+        player = Player()
+        player_expected = Player(100, 1)
+
+        player.check_answer("A", ".-")
+        self.assertEqual(player.score, player_expected.score)
+    def test_wrong_letter(self):
+        player = Player(200, 2)
+        player_expected = Player(100, 0)
+
+        player.check_answer("B", ".-")
+        self.assertEqual(player.score, player_expected.score)
+    def test_number_over_nine(self):
+        player = Player(200, 2)
+        player_expected = Player(100, 0)
+
+        player.check_answer("10", ".-")
+        self.assertEqual(player.score, player_expected.score)
+    def test_number_under_zero(self):
+        player = Player(200, 2)
+        player_expected = Player(100, 0)
+
+        player.check_answer("-14", ".-")
+        self.assertEqual(player.score, player_expected.score)
