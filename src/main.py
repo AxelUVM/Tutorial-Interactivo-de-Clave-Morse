ce21310c7d692d1e956af871bac67c12f16110c3
qAxelFT <axelfloresteja@protonmail.com>
2025-11-05 15:03:14 -0600

    FIX: change layout

--- src/main.py
+++ src/main.py
@@ -1,70 +1,66 @@
 import flet as ft
+import random
 
-morse={ #la lista de la tarduccion del abecedario a morse 
+from player import Player
 
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    
-    "0": "-----",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----."
-    }
-
-# TODO: completar la funcion para revisar la respuesta del usuario
-class Player():
-    def __init__(self, score: int = 0, streak: int = 0, mult: float = 1.0):
-        self.score = score
-        self.streak = streak
-        self.mult = mult
-
-    def check_answer(self, answer: str, code: str):
-        if answer == code:
-            self.score += 100
-            self.streak += 1
-        elif answer != score and score > 0:
-            self.score -= 100
-            self.streak = 0
+morse_list = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+    "-----",
+    ".----",
+    "..---",
+    "...--",
+    "....-",
+    ".....",
+    "-....",
+    "--...",
+    "---..",
+    "----."
+]
 
 def main(page: ft.Page):
+    player = Player()
     # esta variable trackea la cuenta del score
-    score = ft.Text("0", size=25, data=0)
+    score = ft.Text("0", size=25)
+    streak = ft.Text("0", size=25)
+    mult = ft.Text("0.0", size=25)
     # esta variable trackea la clave actual
-    code = ft.Text("placeholder", size = 75)
+    code = ft.Text(morse_list[random.randint(0, len(morse_list)) - 1], size = 75)
     # esta variable contiene la respuesta del usuario
     answer = ft.TextField(label="answer", text_align=ft.TextAlign.CENTER, width=500)
+
+    def btn_callback(e):
+        player.check_answer(answer.value, code.value)
+        score.value = str(player.score)
+        code.value = morse_list[random.randint(0, len(morse_list)) - 1]
+        page.update()
+
     # el boton para mandar la respuesta
-    submit_button = ft.ElevatedButton("Submit", on_click=check_answer)
+    submit_button = ft.ElevatedButton("Submit", on_click=btn_callback)
 
     # construccion de la interfaz
     page.add(
@@ -72,12 +68,7 @@ def main(page: ft.Page):
             content=
             ft.Column(
                 controls=[
-                    ft.Row(
-                        controls=[
-                            ft.Text("Score: ", size=25),
-                            score,
-                        ]
-                    ),
+
                     ft.Container(height=50),
                     ft.Container(
                         code,
@@ -90,7 +81,30 @@ def main(page: ft.Page):
                     ft.Container(
                         submit_button,
                         alignment=ft.alignment.center
-                    )
+                    ),
+                    ft.Column(
+                        controls=[
+                            ft.Row(
+                                controls=[
+                                    ft.Text("Score: ", size=25),
+                                    score,
+                                ],
+                            ),
+                            ft.Row(
+                                controls=[
+                                    ft.Text("Streak: ", size=25),
+                                    streak,
+                                ]
+                            ),
+                            ft.Row(
+                                controls=[
+                                    ft.Text("Mult: ", size=25),
+                                    mult,
+                                    ft.Text("x", size=25),
+                                ]
+                            ),
+                        ],
+                    ),
                 ],
             ),
         )
