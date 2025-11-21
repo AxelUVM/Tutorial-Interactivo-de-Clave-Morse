e15e9cb9250a048801130195d31e5d70d7d07824
qAxelFT <axelfloresteja@protonmail.com>
2025-11-21 04:08:56 -0600

    agregado comentarios

--- src/player.py
+++ src/player.py
@@ -1,55 +1,20 @@
-class Player():
-    morse_dict = {
-        "A": ".-",
-        "B": "-...",
-        "C": "-.-.",
-        "D": "-..",
-        "E": ".",
-        "F": "..-.",
-        "G": "--.",
-        "H": "....",
-        "I": "..",
-        "J": ".---",
-        "K": "-.-",
-        "L": ".-..",
-        "M": "--",
-        "N": "-.",
-        "O": "---",
-        "P": ".--.",
-        "Q": "--.-",
-        "R": ".-.",
-        "S": "...",
-        "T": "-",
-        "U": "..-",
-        "V": "...-",
-        "W": ".--",
-        "X": "-..-",
-        "Y": "-.--",
-        "Z": "--..",
-        "0": "-----",
-        "1": ".----",
-        "2": "..---",
-        "3": "...--",
-        "4": "....-",
-        "5": ".....",
-        "6": "-....",
-        "7": "--...",
-        "8": "---..",
-        "9": "----."
-    }
+# Clase que maneja al jugador
 
+class Player():
+    # definicion de atributos
     def __init__(self, score: int = 0, streak: int = 0, mult: float = 1.0):
         self.score = score
         self.streak = streak
         self.mult = mult
-
-    def check_answer(self, answer: str, code: str):
+    
+    # La funcion que checa si la respuesta es correcta, acepta una respuesta (string) un codigo (string) y el diccionario de respuestas (dependiendo de la dificultad)
+    def check_answer(self, answer: str, code: str, morse_dict: dict):
         try:
-            if self.morse_dict[answer.upper()] == code:
+            if morse_dict[answer.upper()] == code:
                 self.score += 100 * self.mult
                 self.streak += 1
                 self.mult += 0.5
-            elif self.morse_dict[answer.upper()] != code and self.score > 0:
+            elif morse_dict[answer.upper()] != code and self.score > 0:
                 self.score -= 100
                 self.streak = 0
                 self.mult = 1.0
@@ -61,7 +26,8 @@ class Player():
             self.score -= 100
             self.streak = 0
             self.mult = 1.0
-
+    
+    # Funcion que reinicia los atributos cuando se vuelve al menu principal
     def restart_attribs(self):
         self.score = 0
         self.streak = 0
